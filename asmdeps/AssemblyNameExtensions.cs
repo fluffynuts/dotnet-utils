77e038978e9352bb920236c9f1d49fb8d5c9527b
Davyd McColl <davydm@gmail.com>
2023-11-06 09:26:15 +0200

    :sparkles: some minor cleanup

--- asmdeps/AssemblyNameExtensions.cs
+++ asmdeps/AssemblyNameExtensions.cs
@@ -11,10 +11,14 @@ namespace asmdeps
             var version = assemblyName.Version;
             var culture = assemblyName.CultureName.DefaultTo("neutral");
             var token = assemblyName.GetPublicKeyToken().ToHex();
+            var key = assemblyName.GetPublicKey();
             var tokenInfo = string.IsNullOrWhiteSpace(token)
                 ? ""
                 : $", PublicKeyToken={token.BrightRed()}";
-            return $"{name.BrightYellow()}, Version={version.ToString().BrightGreen()}, Culture={culture.BrightCyan()}{tokenInfo}";
+            var keyInfo = (key?.Length ?? 0) == 0
+                ? ", not signed"
+                : $", signed with {key?.ToHex()}";
+            return $"{name.BrightYellow()}, Version={version.ToString().BrightGreen()}, Culture={culture.BrightCyan()}{tokenInfo}{keyInfo}";
         }
     }
 }
